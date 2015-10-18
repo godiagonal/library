d72ac0b465499092bee83a39f03660c934b37c6e
Samuel Johansson <same_johansson@hotmail.com>
2015-10-18 16:53:06 +0200

    Format

--- Library/Models/Book.cs
+++ Library/Models/Book.cs
@@ -4,14 +4,17 @@ using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
-namespace Library.Models {
-    public class Book {
+namespace Library.Models
+{
+    public class Book
+    {
         [Key]
         public int Id { get; set; }
         [Required]
         public string Title { get; set; }
 
-        public override string ToString() {
+        public override string ToString()
+        {
             return String.Format("[{0}] -- {1}", this.Id, this.Title);
         }
     }
