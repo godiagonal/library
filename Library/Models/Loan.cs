8109f52543b12f80fc3c8554951358e66624eeb3
fettavskiljarn <lukaspeterson@hotmail.com>
2015-10-19 11:05:33 +0200

    asdsa

---
+++ Library/Models/Loan.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Library.Models
+{
+    class Loan
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public DateTime TimeOfLoan { get; set; }
+        public DateTime DueDate { get; set; }
+        public DateTime? TimeOfReturn { get; set; }
+        public virtual BookCopy BookCopy {get; set;}
+        public virtual Member Member {get; set;}
+    }
+}
